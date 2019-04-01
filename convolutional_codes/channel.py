253e86a4a71a4d472096e2193a9876440f2ab7b3
Nacho <nachocmstrato@gmail.com>
2019-04-01 17:25:18 +0200

    a

--- convolutional_codes/channel.py
+++ convolutional_codes/channel.py
@@ -2,7 +2,8 @@ import numpy as np
 
 def binary_symmetric(sequences, Pe):
 	aux_seq = np.asarray(sequences).ravel()
-	flip_idx = np.random.randint(2, size=len(aux_seq)) > Pe
+	flip_idx = np.random.choice([0, 1], size=len(aux_seq), p=[1-Pe, Pe])
+	#flip_idx = np.random.randint(2, size=len(aux_seq)) < Pe
 	output = np.logical_xor(list(map(bool,aux_seq)),flip_idx)
 
 	return output.astype(int)
